fa38df86d3f72b7bc19ef17162212fcf2a482955
nonokeks <norina.marie.grosch@uni-weimar.de>
2015-11-02 20:04:21 +0100

    EuroToDollarConverter

---
+++ eurotodollarconverter.cpp
@@ -0,0 +1,22 @@
+#include "eurotodollarconverter.hpp"
+
+#include <string>
+
+EuroToDollarConverter::EuroToDollarConverter()
+{
+}
+
+/*In: double value of euro
+ *Out: dollar value of input euro 31.10.2105
+ */
+double EuroToDollarConverter::convert(double inEuro){
+  return inEuro * 1.1009;
+}
+
+std::string EuroToDollarConverter::toString() const{
+  return "Euro To Dollar Converter";
+}
+
+void EuroToDollarConverter::print() const{
+  std::cout << toString();
+}
---
+++ eurotodollarconverter.hpp
@@ -0,0 +1,15 @@
+#ifndef EUROTODOLLARCONVERTER_H
+#define EUROTODOLLARCONVERTER_H
+
+#include "currencyconverter.hpp"
+
+class EuroToDollarConverter : public CurrencyConverter
+{
+  public:
+    EuroToDollarConverter();
+    double convert(double inCelsius);
+    std::string toString() const;
+    void print() const;
+};
+
+#endif // EUROTODOLLARCONVERTER_H
