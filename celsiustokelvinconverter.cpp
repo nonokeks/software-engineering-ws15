a1ced501feef9cca8a052fb0c6c0e5455459b1d2
nonokeks <norina.marie.grosch@uni-weimar.de>
2015-11-02 20:10:08 +0100

    CelsiusToKelvinConverter

---
+++ celsiustokelvinconverter.cpp
@@ -0,0 +1,22 @@
+#include "celsiustokelvinconverter.hpp"
+
+#include <string>
+
+CelsiusToKelvinConverter::CelsiusToKelvinConverter()
+{
+}
+
+/*In: double value of celsius
+ *Out: kelvin value of input celsius
+ */
+double CelsiusToKelvinConverter::convert(double inCelsius){
+  return inCelsius + 273.15;
+}
+
+std::string CelsiusToKelvinConverter::toString() const{
+  return "Celsius To Kelvin Converter";
+}
+
+void CelsiusToKelvinConverter::print() const{
+  std::cout << toString();
+}
---
+++ celsiustokelvinconverter.hpp
@@ -0,0 +1,15 @@
+#ifndef CELSIUSTOKELVINCONVERTER_H
+#define CELSIUSTOKELVINCONVERTER_H
+
+#include "temperatureconverter.hpp"
+
+class CelsiusToKelvinConverter : public TemperatureConverter
+{
+  public:
+    CelsiusToKelvinConverter();
+    double convert(double inCelsius);
+    std::string toString() const;
+    void print() const;
+};
+
+#endif // CELSIUSTOKELVINCONVERTER_H
